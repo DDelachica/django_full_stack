9db66dde19427fa8a47a02c6f95e016343e022c7
Daniel <ddelachica89@gmail.com>
2020-04-26 16:02:46 -0500

    added validations

--- log_reg_proj/log_reg_app/models.py
+++ log_reg_proj/log_reg_app/models.py
@@ -1,7 +1,30 @@
 from django.db import models
+import re
+
+
+class UserManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        email_checker = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
+        if not postData['fname'].isalpha():
+            errors['fname'] = "First name cannot have numbers"
+        if not postData['lname'].isalpha():
+            errors['lname'] = "Last name cannot have numbers"
+        if not email_checker.match(postData['email']):    
+            errors['email'] = ("Invalid email address!")
+        if len(postData['fname']) < 2:
+            errors["fname"] = "First name should be at least 2 characters"
+        if len(postData['lname']) < 2:
+            errors["lname"] = "Last name should be at least 2 characters"
+        if len(postData['pw']) < 8:
+            errors['pw'] = "Password should be at least 8 characters"
+        if postData['pw'] != postData['confpw']:
+            errors['pw'] = 'Password and Confirm Password do not match'
+        return errors
 
 class User(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
+    objects = UserManager()
--- log_reg_proj/log_reg_app/views.py
+++ log_reg_proj/log_reg_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import User
+from django.contrib import messages
 
 def index(request):
     return render(request, 'index.html')
@@ -11,12 +12,14 @@ def success(request):
 
 def register(request):
     print(request.POST)
-    if request.POST['pw'] != request.POST['confpw']:
+    errors = User.objects.basic_validator(request.POST)
+    if len(errors)>0:
+        for key, value in errors.items():
+            messages.error(request, value)
         return redirect('/')
-    else:
-        new_user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=request.POST['pw'])
-        request.session['user'] = new_user.first_name
-        return redirect('/success')
+    new_user = User.objects.create(first_name=request.POST['fname'], last_name=request.POST['lname'], email=request.POST['email'], password=request.POST['pw'])
+    request.session['user'] = new_user.first_name
+    return redirect('/success')
 
 def login(request):
     print(request.POST)
